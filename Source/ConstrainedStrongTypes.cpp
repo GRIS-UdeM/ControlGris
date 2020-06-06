a6cf0aa59d5a96396b425d8c5be9584602941951
Samuel Beland <samuel.beland@gmail.com>
2020-06-06 09:48:32 -0400

    Fixed sin-cos invertion.

--- Source/ConstrainedStrongTypes.cpp
+++ Source/ConstrainedStrongTypes.cpp
@@ -11,6 +11,6 @@
 #include "ConstrainedStrongTypes.h"
 
 constexpr SourcePosition::SourcePosition(Radians const & angle, float const radius)
-    : mX(ClippedPosition{std::sin(static_cast<float>(angle)) * radius})
-    , mY(ClippedPosition{std::cos(static_cast<float>(angle)) * radius})
+    : mX(ClippedPosition{std::cos(static_cast<float>(angle)) * radius})
+    , mY(ClippedPosition{std::sin(static_cast<float>(angle)) * radius})
 {}
--- Source/Source.cpp
+++ Source/Source.cpp
@@ -147,7 +147,7 @@ void Source::computeXY()
         }
     }()};
 
-    mPosition = Point<float>{ std::sin(mAzimuth), std::cos(mAzimuth) } * radius;
+    mPosition = Point<float>{ std::cos(mAzimuth), std::sin(mAzimuth) } * radius;
 
     this->sendChangeMessage();
 }
@@ -162,7 +162,7 @@ void Source::computeAzimuthElevation()
     if (mSpatMode == SpatMode::dome) { // azimuth - elevation
         auto const clippedRadius{ std::min(radius, 1.0f) };
         if (clippedRadius < radius) {
-            mPosition = Point<float>{ std::sin(mAzimuth), std::cos(mAzimuth)} * radius;
+            mPosition = Point<float>{ std::cos(mAzimuth), std::sin(mAzimuth)} * radius;
         }
         auto const elevation{ halfPi * clippedRadius };
         mElevation = elevation;
