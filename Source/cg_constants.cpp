ce982122a5712a61ae4243dccae4c7af8520c04d
Samuel Béland <samuel.beland@gmail.com>
2021-07-22 09:56:10 -0400

    Renamed elevation source links (issue #163).

--- Source/cg_constants.cpp
+++ Source/cg_constants.cpp
@@ -62,4 +62,59 @@ Automation::Enum Automation::idToEnum(const juce::String & name)
     return {};
 }
 
+juce::String const SOURCE_SELECTION_WARNING{ "This source link does not allow individual moves." };
+
+juce::StringArray const SOURCE_PLACEMENT_SKETCH{
+    juce::String("Left Alternate"),  juce::String("Right Alternate"),
+    juce::String("Left Clockwise"),  juce::String("Left Counter Clockwise"),
+    juce::String("Right Clockwise"), juce::String("Right Counter Clockwise"),
+    juce::String("Top Clockwise"),   juce::String("Top Counter Clockwise")
+};
+
+juce::StringArray const POSITION_SOURCE_LINK_TYPES{
+    juce::String("Independent"),           juce::String("Circular"),
+    juce::String("Circular Fixed Radius"), juce::String("Circular Fixed Angle"),
+    juce::String("Circular Fully Fixed"),  juce::String("Delta Lock"),
+    juce::String("Symmetric X"),           juce::String("Symmetric Y")
+};
+
+juce::StringArray const ELEVATION_SOURCE_LINK_TYPES{ juce::String("Independent"),
+                                                     juce::String("Equal Elevation"),
+                                                     juce::String("Bottom-Top"),
+                                                     juce::String("Top-Bottom"),
+                                                     juce::String("Delta Lock") };
+
+juce::StringArray const POSITION_TRAJECTORY_TYPE_TYPES{ juce::String("Realtime"),
+                                                        juce::String("Drawing"),
+                                                        juce::String("Circle Clockwise"),
+                                                        juce::String("Circle Counter Clockwise"),
+                                                        juce::String("Ellipse Clockwise"),
+                                                        juce::String("Ellipse Counter Clockwise"),
+                                                        juce::String("Spiral Clockwise Out In"),
+                                                        juce::String("Spiral Counter Clockwise Out In"),
+                                                        juce::String("Spiral Clockwise In Out"),
+                                                        juce::String("Spiral Counter Clockwise In Out"),
+                                                        juce::String("Square Clockwise"),
+                                                        juce::String("Square Counter Clockwise"),
+                                                        juce::String("Triangle Clockwise"),
+                                                        juce::String("Triangle Counter Clockwise") };
+
+juce::StringArray const ELEVATION_TRAJECTORY_TYPE_TYPES{ juce::String("Realtime"),
+                                                         juce::String("Drawing"),
+                                                         juce::String("Up Down"),
+                                                         juce::String("Down Up") };
+
+juce::StringArray const FIXED_POSITION_DATA_HEADERS{
+    // TODO: is this useful?
+    juce::String("ID"),   juce::String("S1_X"), juce::String("S1_Y"), juce::String("S1_Z"), juce::String("S2_X"),
+    juce::String("S2_Y"), juce::String("S2_Z"), juce::String("S3_X"), juce::String("S3_Y"), juce::String("S3_Z"),
+    juce::String("S4_X"), juce::String("S4_Y"), juce::String("S4_Z"), juce::String("S5_X"), juce::String("S5_Y"),
+    juce::String("S5_Z"), juce::String("S6_X"), juce::String("S6_Y"), juce::String("S6_Z"), juce::String("S7_X"),
+    juce::String("S7_Y"), juce::String("S7_Z"), juce::String("S8_X"), juce::String("S8_Y"), juce::String("S8_Z"),
+    juce::String("T1_X"), juce::String("T1_Y"), juce::String("T1_Z"), juce::String("T2_X"), juce::String("T2_Y"),
+    juce::String("T2_Z")
+};
+
+juce::String const FIXED_POSITION_DATA_TAG("Fix_Position_Data");
+
 } // namespace gris
--- Source/cg_constants.hpp
+++ Source/cg_constants.hpp
@@ -39,7 +39,7 @@ constexpr Radians MAX_ELEVATION = Degrees{ 90.0f };
 // Spatialisation modes.
 enum class SpatMode { dome, cube };
 
-static juce::String const SOURCE_SELECTION_WARNING{ "This source link does not allow individual moves." };
+extern juce::String const SOURCE_SELECTION_WARNING;
 
 //==============================================================================
 // Configuration -> Source tab -> Source Placement popup choices.
@@ -54,12 +54,9 @@ enum class SourcePlacement {
     topClockwise,
     topCounterClockwise
 };
-juce::StringArray const SOURCE_PLACEMENT_SKETCH{
-    juce::String("Left Alternate"),  juce::String("Right Alternate"),
-    juce::String("Left Clockwise"),  juce::String("Left Counter Clockwise"),
-    juce::String("Right Clockwise"), juce::String("Right Counter Clockwise"),
-    juce::String("Top Clockwise"),   juce::String("Top Counter Clockwise")
-};
+
+extern juce::StringArray const SOURCE_PLACEMENT_SKETCH;
+;
 
 //==============================================================================
 // Trajectories -> Source Link popup choices.
@@ -74,19 +71,11 @@ enum class PositionSourceLink {
     symmetricX,
     symmetricY
 };
-juce::StringArray const POSITION_SOURCE_LINK_TYPES{
-    juce::String("Independent"),           juce::String("Circular"),
-    juce::String("Circular Fixed Radius"), juce::String("Circular Fixed Angle"),
-    juce::String("Circular Fully Fixed"),  juce::String("Delta Lock"),
-    juce::String("Symmetric X"),           juce::String("Symmetric Y")
-};
+
+extern juce::StringArray const POSITION_SOURCE_LINK_TYPES;
 
 enum class ElevationSourceLink { undefined, independent, fixedElevation, linearMin, linearMax, deltaLock };
-juce::StringArray const ELEVATION_SOURCE_LINK_TYPES{ juce::String("Independent"),
-                                                     juce::String("Fixed Elevation"),
-                                                     juce::String("Linear Min"),
-                                                     juce::String("Linear Max"),
-                                                     juce::String("Delta Lock") };
+extern juce::StringArray const ELEVATION_SOURCE_LINK_TYPES;
 
 //==============================================================================
 // Trajectories -> Trajectory Type popup choices.
@@ -107,41 +96,18 @@ enum class PositionTrajectoryType {
     triangleClockwise,
     triangleCounterClockwise
 };
-juce::StringArray const POSITION_TRAJECTORY_TYPE_TYPES{ juce::String("Realtime"),
-                                                        juce::String("Drawing"),
-                                                        juce::String("Circle Clockwise"),
-                                                        juce::String("Circle Counter Clockwise"),
-                                                        juce::String("Ellipse Clockwise"),
-                                                        juce::String("Ellipse Counter Clockwise"),
-                                                        juce::String("Spiral Clockwise Out In"),
-                                                        juce::String("Spiral Counter Clockwise Out In"),
-                                                        juce::String("Spiral Clockwise In Out"),
-                                                        juce::String("Spiral Counter Clockwise In Out"),
-                                                        juce::String("Square Clockwise"),
-                                                        juce::String("Square Counter Clockwise"),
-                                                        juce::String("Triangle Clockwise"),
-                                                        juce::String("Triangle Counter Clockwise") };
+
+extern juce::StringArray const POSITION_TRAJECTORY_TYPE_TYPES;
+;
 
 enum class ElevationTrajectoryType { undefined, realtime, drawing, downUp, upDown };
-juce::StringArray const ELEVATION_TRAJECTORY_TYPE_TYPES{ juce::String("Realtime"),
-                                                         juce::String("Drawing"),
-                                                         juce::String("Up Down"),
-                                                         juce::String("Down Up") };
+extern juce::StringArray const ELEVATION_TRAJECTORY_TYPE_TYPES;
 
 //==============================================================================
 // Fix position data headers.
-juce::StringArray const FIXED_POSITION_DATA_HEADERS{
-    // TODO: is this useful?
-    juce::String("ID"),   juce::String("S1_X"), juce::String("S1_Y"), juce::String("S1_Z"), juce::String("S2_X"),
-    juce::String("S2_Y"), juce::String("S2_Z"), juce::String("S3_X"), juce::String("S3_Y"), juce::String("S3_Z"),
-    juce::String("S4_X"), juce::String("S4_Y"), juce::String("S4_Z"), juce::String("S5_X"), juce::String("S5_Y"),
-    juce::String("S5_Z"), juce::String("S6_X"), juce::String("S6_Y"), juce::String("S6_Z"), juce::String("S7_X"),
-    juce::String("S7_Y"), juce::String("S7_Z"), juce::String("S8_X"), juce::String("S8_Y"), juce::String("S8_Z"),
-    juce::String("T1_X"), juce::String("T1_Y"), juce::String("T1_Z"), juce::String("T2_X"), juce::String("T2_Y"),
-    juce::String("T2_Z")
-};
+extern juce::StringArray const FIXED_POSITION_DATA_HEADERS;
 
-juce::String const FIXED_POSITION_DATA_TAG("Fix_Position_Data");
+extern juce::String const FIXED_POSITION_DATA_TAG;
 
 namespace Automation
 {
